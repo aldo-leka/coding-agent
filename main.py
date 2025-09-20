74af181bcdd3a8dd53e7bb7398677c87465cbb0a
Aldo Leka <aldo.leka@live.com>
2025-09-20 11:15:39 +0200

    add --verbose flag

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ if len(sys.argv) < 2:
     sys.exit(1)
 
 user_prompt = sys.argv[1]
+verbose = False
+if (len(sys.argv) > 2):
+    verbose = sys.argv[2] == "--verbose"
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -20,6 +23,10 @@ messages = [
 ]
 
 response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+
 print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+if verbose:
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
