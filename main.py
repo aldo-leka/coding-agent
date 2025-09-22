ae6aee347c020eea524bac74443868ad4a126a0a
Aldo Leka <aldo.leka@live.com>
2025-09-22 15:31:21 +0200

    finito

--- main.py
+++ main.py
@@ -3,10 +3,57 @@ import sys
 from google import genai
 from dotenv import load_dotenv
 from google.genai import types
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.run_python_file import schema_run_python_file
-from functions.write_file import schema_write_file
+from functions.get_files_info import schema_get_files_info, get_files_info
+from functions.get_file_content import schema_get_file_content, get_file_content
+from functions.run_python_file import schema_run_python_file, run_python_file
+from functions.write_file import schema_write_file, write_file
+
+functions = {
+        "get_files_info": get_files_info,
+        "get_file_content": get_file_content,
+        "run_python_file": run_python_file,
+        "write_file": write_file
+    }
+
+def call_function(function_call_part, verbose=False):
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f" - Calling function: {function_call_part.name}")
+
+    function_name = function_call_part.name
+    function_args = function_call_part.args
+
+    if function_name not in functions:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"error": f"Unknown function: {function_name}"},
+                )
+            ],
+        )
+    
+    function_result = ""
+    if function_name == "get_files_info":
+        function_result = get_files_info("./calculator", **function_args)
+    elif function_name == "get_file_content":
+        function_result = get_file_content("./calculator", **function_args)
+    elif function_name == "run_python_file":
+        function_result = run_python_file("./calculator", **function_args)
+    else:
+        function_result = write_file("./calculator", **function_args)
+    
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_name,
+                response={"result": function_result},
+            )
+        ],
+    )
 
 if len(sys.argv) < 2:
     print("expecting 'contents' argument")
@@ -57,7 +104,9 @@ response = client.models.generate_content(
     ))
 
 for function_call_part in response.function_calls:
-    print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    function_call_result = call_function(function_call_part, verbose)
+    if verbose:
+        print(f"-> {function_call_result.parts[0].function_response.response}")
 
 if verbose:
     print(f"User prompt: {user_prompt}")
