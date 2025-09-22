1e1565bff3dae7c1450b025fe990c476731ec8ce
Aldo Leka <aldo.leka@live.com>
2025-09-22 11:29:19 +0200

    add write_file

---
+++ functions/write_file.py
@@ -0,0 +1,22 @@
+import os
+
+def write_file(working_directory, file_path, content):
+    try:
+        joined_path = os.path.join(working_directory, file_path)
+        abs_target = os.path.abspath(joined_path)
+        abs_working = os.path.abspath(working_directory)
+
+        if not (abs_target.startswith(abs_working + os.sep) or abs_target == abs_working):
+            raise Exception(f'Cannot read "{file_path}" as it is outside the permitted working directory')
+        
+        with open(abs_target, "w" if os.path.exists(abs_target) else "x") as f:
+            f.write(content)
+
+        msg = f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+        print(msg)
+        return msg
+
+    except Exception as e:
+        err = f"Error: {str(e)}"
+        print(err)
+        return err
